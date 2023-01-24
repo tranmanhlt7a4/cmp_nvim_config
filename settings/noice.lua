10182fd1c8103eb2cd16e3d233a0adeefd0f70bc
Trần Quang Mạnh <manhtran050805@gmail.com>
2023-01-24 16:36:53 +0700

    Update noice.lua

--- settings/noice.lua
+++ settings/noice.lua
@@ -1,4 +1,5 @@
 require("noice").setup({
+  lsp = {
     signature = {
       enabled = false,
       auto_open = {
@@ -11,20 +12,25 @@ require("noice").setup({
       ---@type NoiceViewOptions
       opts = {}, -- merged with defaults from documentation
     },
-    lsp = {
+    hover = {
+        enabled = false,
+        view = nil, -- when nil, use defaults from documentation
+        ---@type NoiceViewOptions
+        opts = {}, -- merged with defaults from documentation
+      },
       -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
         ["cmp.entry.get_documentation"] = true,
       },
-    },
-    -- you can enable a preset for easier configuration
-    presets = {
-      bottom_search = false, -- use a classic bottom cmdline for search
-      command_palette = false, -- position the cmdline and popupmenu together
-      long_message_to_split = false, -- long messages will be sent to a split
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = false, -- add a border to hover docs and signature help
-    },
-  })
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = false, -- use a classic bottom cmdline for search
+    command_palette = false, -- position the cmdline and popupmenu together
+    long_message_to_split = false, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+})
